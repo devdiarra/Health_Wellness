a69b1e7881d036a720aa5ae6320b440086294865
Moustapha Diarra <diarra.msa@gmail.com>
2023-09-07 02:37:33 +0300

    Build peer-therapist page

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
+
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    #
+    path('peer/', TemplateView.as_view(template_name='pages/peer.html'), name='peer')
 ]
